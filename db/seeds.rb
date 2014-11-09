032dcd488b2d729cb22c379d4a3dc56e31a9f9ae
ohad stauber <ohad.stauber@gmail.com>
2014-11-09 04:01:40 +0000

    try 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true,
+             admin:     true,
              activated: true,
              activated_at: Time.zone.now)
 
@@ -11,11 +11,11 @@ User.create!(name:  "Example User",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
 
 users = User.order(:created_at).take(6)
