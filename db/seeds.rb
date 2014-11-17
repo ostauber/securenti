121107d07790fc5304aafc6ba94dd2a45429d439
ohad stauber <ohad.stauber@gmail.com>
2014-11-17 05:02:32 +0000

    all good

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 # Users
+User.create!(name:  "Ohad Stauber",
+             email: "ohad.stauber@gmail.com",
+             password:              "shshsh",
+             password_confirmation: "shshsh",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
